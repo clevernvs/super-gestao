c4e1a7842be91f1c5a2aaa512e5964a94e49e727
Cleverton  Neves <clever.nvs@gmail.com>
2023-05-10 21:34:05 -0300

    Configurando o bootstrap

--- resources/views/adm/template/basico.blade.php
+++ resources/views/adm/template/basico.blade.php
@@ -8,7 +8,7 @@
     <title>Super Gestão - ADM - @yield('pageTitle')</title>
     <!-- <link rel="stylesheet" href="{{ asset('css/estilo.css') }}"> -->
     <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> --}}
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
